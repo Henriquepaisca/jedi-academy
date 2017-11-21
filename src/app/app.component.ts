bea2867a5e965709e235d33e707a285522dc383e
Henriquepaisca <carlospaisca@gmail.com>
2017-11-21 01:04:43 -0200

    primeiro componente Header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'jad-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'jad';
+  title = 'jad works!';
 }
